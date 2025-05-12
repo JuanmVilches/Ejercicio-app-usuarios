17617351d2d5b372f9847ac714ee6393ee222971
Juan Manuel Vilches <juanmanuelvilches99@gmail.com>
2025-05-11 23:52:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const users = [{}]
+
+const tableHTML = document.getElementById("user-table")
+const formHTML = document.getElementById("user-form")
+
+console.log(formHTML)
+console.log(tableHTML)
+
+formHTML.addEventListener("submit", (evt)=>{
+
+})
