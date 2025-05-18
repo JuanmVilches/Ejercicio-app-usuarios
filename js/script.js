0c49154d991fd92873ca8078f6fbdcbff96f5789
Juan Manuel Vilches <juanmanuelvilches99@gmail.com>
2025-05-18 15:19:06 -0300

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-const users = []
+const users = [
+    {
+        id: Date.now().toString().slice(-5),
+        name: "Juan Manuel Vilches",
+        email: "email@gmail.com",
+        phone: 123456789,
+        date: "1999-09-03",
+    }
+]
 
 const tableBodyHTML = document.getElementById("table-body")
 const formHTML = document.getElementById("user-form")
@@ -19,6 +27,13 @@ formHTML.addEventListener("submit", (evt) => {
 
     users.push(newUser)
     pintarUsuarios(users)
+
+    Swal.fire({
+        title: "Usuario añadido",
+        text: "El usuario fue agregado correctamente",
+        icon: "success",
+    })
+    formHTML.reset()
 })
 
 function pintarUsuarios(arrayUsers) {
@@ -35,37 +50,54 @@ function pintarUsuarios(arrayUsers) {
                                 <td>${user.date}</td>
                                 <td>
                                     <div class="action-group">
-                                        <button onclick="borrarUser(${user.id})">
-                                            <i class="fa-solid fa-trash action-btn"></i>
+                                        <button title="Borrar usuario" class="button-icon trash"onclick="borrarUser(${user.id})">
+                                            <i class="fa-solid fa-trash"></i>
                                         </button>
-                                        <button>
-                                            <i class="fa-solid fa-pen action-btn"></i>
+                                        <button title="Editar usuario" class="button-icon" onclick="editarUser(${user.id})">
+                                            <i class="fa-solid fa-pen"></i>
                                         </button>
                                     </div>
 
                                 </td>
                             </tr>`
     });
-}
 
+}
+pintarUsuarios(users)
 function borrarUser(id) {
-    console.log(id)
 
     const indiceUser = users.findIndex(user => {
         if (Number(user.id) === id) {
             return true
         }
     })
+    Swal.fire({
+        title: "¿Esta seguro que desea borrar el usuario?",
+        text: "No podrá deshacer esta acción",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, borrar usuario!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Borrado!",
+                text: "El usuario fue borrado correctamente",
+                icon: "success"
+            });
+            users.splice(indiceUser, 1)
 
-    console.log (indiceUser)
-    const confirmar = confirm("Desea borrar el usuario?")
+            pintarUsuarios(users)
+        }
+    });
 
 
-    if (confirmar) {
-        users.splice(indiceUser, 1)
+    // if (confirmar) {
+    //     users.splice(indiceUser, 1)
 
-        pintarUsuarios(users)
-    }
+    //     pintarUsuarios(users)
+    // }
 }
 
 
